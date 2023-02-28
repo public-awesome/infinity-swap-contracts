d07af0ed154eff96733371c88ca1f64becc15812
Tasio Victoria <tasiovictoria@gmail.com>
2023-02-28 14:35:47 -0500

    Write test for creating large pools

--- e2e/src/tests/pool_test.rs
+++ e2e/src/tests/pool_test.rs
@@ -28,9 +28,9 @@ fn test_small_pool_creation(chain: &mut Chain) {
 
     // gen user that will:
     // * init minter
-    // * mint 1 NFT
+    // * mint 2 NFTs
     // * create Trade pool
-    // * deposit 1 NFT
+    // * deposit 2 NFTs
     // * deposit 1_000_000 tokens
     // * activate pool
     // Min balance: MINT_PRICE + LISTING_FEE + 1_000_000 tokens
@@ -52,7 +52,7 @@ fn test_small_pool_creation(chain: &mut Chain) {
     let collection = chain.orc.contract_map.address(SG721_NAME).unwrap();
 
     let resp = mint_nfts(chain, 2, &user);
-    let tag = resp
+    let tag = resp[0]
         .res
         .find_event_tags("wasm".to_string(), "token_id".to_string());
     let token_ids = tag
@@ -135,7 +135,6 @@ fn test_small_pool_creation(chain: &mut Chain) {
         },
     );
     let resp_pool = resp.pools[0].1.clone().unwrap();
-    println!("{:?}", resp_pool);
     assert_eq!(
         resp_pool,
         Pool {
@@ -155,5 +154,131 @@ fn test_small_pool_creation(chain: &mut Chain) {
             reinvest_tokens: true,
             reinvest_nfts: true,
         }
+    );
+}
+
+#[test_context(Chain)]
+#[test]
+#[ignore]
+fn test_large_pool_creation(chain: &mut Chain) {
+    let denom = chain.cfg.orc_cfg.chain_cfg.denom.clone();
+    let prefix = chain.cfg.orc_cfg.chain_cfg.prefix.clone();
+
+    // create minter with master account
+    let master_account = chain.cfg.users[1].clone();
+
+    // gen user that will:
+    // * init minter
+    // * mint 10_000 NFT
+    // * create Trade pool
+    // * deposit 10_000 NFT
+    // * deposit 1_000_000_000_000 tokens
+    // * activate pool
+    // Min balance: MINT_PRICE + LISTING_FEE + 1_000_000_000_000 tokens
+    let pool_deposit_amount = 1_000_000_000_000;
+    let balance = (MINT_PRICE + LISTING_FEE + pool_deposit_amount) * 2;
+    let user = gen_users(chain, 1, balance)[0].clone();
+    let user_addr = user.to_addr(&prefix).unwrap();
+
+    // init minter
+    instantiate_minter(
+        &mut chain.orc,
+        // set creator address as user to allow for minting on base minter
+        user_addr.to_string(),
+        &master_account.key,
+        &denom,
+    )
+    .unwrap();
+
+    let collection = chain.orc.contract_map.address(SG721_NAME).unwrap();
+
+    let resp = mint_nfts(chain, 10_000, &user);
+    let mut token_ids: Vec<String> = vec![];
+
+    for res in resp {
+        let tags = res
+            .res
+            .find_event_tags("wasm".to_string(), "token_id".to_string());
+        token_ids.append(
+            &mut tags
+                .iter()
+                .map(|tag| tag.value.clone())
+                .collect::<Vec<String>>(),
+        );
+    }
+
+    approve_all_nfts(
+        chain,
+        chain.orc.contract_map.address(INFINITY_POOL_NAME).unwrap(),
+        &user,
+    );
+
+    let resp = pool_execute_message(
+        chain,
+        InfinityPoolExecuteMsg::CreatePool {
+            collection: collection.clone(),
+            asset_recipient: None,
+            pool_type: PoolType::Trade,
+            bonding_curve: BondingCurve::ConstantProduct,
+            spot_price: Uint128::zero(),
+            delta: Uint128::zero(),
+            finders_fee_bps: 0,
+            swap_fee_bps: 100,
+            reinvest_tokens: true,
+            reinvest_nfts: true,
+        },
+        "infinity_pool_create_pool",
+        vec![OrcCoin {
+            amount: LISTING_FEE,
+            denom: denom.parse().unwrap(),
+        }],
+        &user,
+    );
+
+    let tag = resp
+        .res
+        .find_event_tags("wasm-create_token_pool".to_string(), "id".to_string())[0];
+
+    let pool_id = tag.value.parse::<u64>().unwrap();
+
+    pool_execute_message(
+        chain,
+        InfinityPoolExecuteMsg::DepositNfts {
+            pool_id,
+            collection: collection.clone(),
+            nft_token_ids: token_ids.clone(),
+        },
+        "infinity_pool_deposit_nfts",
+        vec![],
+        &user,
+    );
+
+    pool_execute_message(
+        chain,
+        InfinityPoolExecuteMsg::DepositTokens { pool_id },
+        "infinity_pool_deposit_tokens",
+        vec![OrcCoin {
+            amount: pool_deposit_amount,
+            denom: denom.parse().unwrap(),
+        }],
+        &user,
+    );
+
+    pool_execute_message(
+        chain,
+        InfinityPoolExecuteMsg::SetActivePool {
+            is_active: true,
+            pool_id,
+        },
+        "infinity_pool_activate",
+        vec![],
+        &user,
+    );
+
+    let resp: PoolsByIdResponse = pool_query_message(
+        chain,
+        InfinityPoolQueryMsg::PoolsById {
+            pool_ids: vec![pool_id],
+        },
     );
 }
