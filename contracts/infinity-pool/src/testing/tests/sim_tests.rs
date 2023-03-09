a76d2cf3b0e9465da84ade7df07b296e877ca9cd
Humanalgorithm <scottomj@gmail.com>
2023-03-09 01:10:02 -0500

    saving version that works with looping through swap results

--- contracts/infinity-pool/src/testing/tests/sim_tests.rs
+++ contracts/infinity-pool/src/testing/tests/sim_tests.rs
@@ -2,4 +2,5 @@ mod helpers;
 mod sim_direct_swap_nfts_for_tokens;
 mod sim_direct_swap_tokens_for_specific_nfts;
 mod sim_swap_nfts_for_tokens;
+mod sim_swap_tokens_for_any_nfts;
 mod sim_swap_tokens_for_specific_nfts;
