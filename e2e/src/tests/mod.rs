c057b544c6560dd41afc9579b6b840f38dc76694
Tasio Victoria <tasiovictoria@gmail.com>
2023-03-18 22:53:16 -0400

    Add swap_tokens_for_any_nfts e2e test

--- e2e/src/tests/mod.rs
+++ e2e/src/tests/mod.rs
@@ -2,4 +2,5 @@ pub mod direct_swap_nfts_for_tokens_test;
 pub mod direct_swap_tokens_for_specific_nfts;
 pub mod pool_tests;
 pub mod swap_nfts_for_tokens_test;
+pub mod swap_tokens_for_any_nfts;
 pub mod swap_tokens_for_specific_nfts;
