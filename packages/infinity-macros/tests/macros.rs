7ecb3cbb97ec6b6da5328738dba9e78bc8dae263
Tasio Victoria <tasiovictoria@gmail.com>
2023-04-26 17:02:28 -0400

    Rename infinity-interface to infinity-shared

--- packages/infinity-macros/tests/macros.rs
+++ packages/infinity-macros/tests/macros.rs
@@ -1,16 +1,13 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{Timestamp, Uint128};
-use infinity_interface::{NftOrder, SwapParams};
-use infinity_macros::infinity_module_query;
+use infinity_macros::{infinity_module_execute, infinity_module_query};
+use infinity_shared::interface::{NftOrder, SwapParams};
 
-/// enum for testing. Important that this derives things / has other
-/// attributes so we can be sure we aren't messing with other macros
-/// with ours.
 #[infinity_module_query]
 #[allow(dead_code)]
 #[cw_serde]
 #[derive(QueryResponses)]
-enum Test {
+enum TestQueryMsg {
     #[returns(String)]
     Foo,
     #[returns(String)]
@@ -32,28 +29,74 @@ fn infinity_module_query_derive() {
         asset_recipient: Some("asset_recipient".to_string()),
         finder: Some("finder".to_string()),
     };
-    Test::SimSwapTokensForAnyNfts {
+    TestQueryMsg::SimSwapTokensForAnyNfts {
         sender: "sender".to_string(),
         collection: "collection".to_string(),
         orders: vec![Uint128::from(10u64)],
         swap_params: swap_params.clone(),
     };
-    Test::SimSwapNftsForTokens {
+    TestQueryMsg::SimSwapNftsForTokens {
         sender: "sender".to_string(),
         collection: "collection".to_string(),
         nft_orders: nft_orders.clone(),
         swap_params: swap_params.clone(),
     };
 
-    let test = Test::Foo {};
+    let test = TestQueryMsg::Foo {};
 
     // If this compiles we have won.
     let result = match test {
-        Test::Foo
-        | Test::Bar(_)
-        | Test::Baz { .. }
-        | Test::SimSwapTokensForAnyNfts { .. }
-        | Test::SimSwapNftsForTokens { .. } => "yay",
+        TestQueryMsg::Foo
+        | TestQueryMsg::Bar(_)
+        | TestQueryMsg::Baz { .. }
+        | TestQueryMsg::SimSwapTokensForAnyNfts { .. }
+        | TestQueryMsg::SimSwapNftsForTokens { .. } => "yay",
+    };
+    assert_eq!(result, "yay");
+}
+
+#[infinity_module_execute]
+#[allow(dead_code)]
+#[cw_serde]
+enum TestExecuteMsg {
+    Foo,
+    Bar(u64),
+    Baz { waldo: u64 },
+}
+
+#[test]
+fn infinity_module_execute_derive() {
+    let nft_orders: Vec<NftOrder> = vec![NftOrder {
+        token_id: "1".to_string(),
+        amount: Uint128::from(10u64),
+    }];
+
+    let swap_params = SwapParams {
+        deadline: Timestamp::from_seconds(10),
+        robust: true,
+        asset_recipient: Some("asset_recipient".to_string()),
+        finder: Some("finder".to_string()),
+    };
+    TestExecuteMsg::SwapTokensForAnyNfts {
+        collection: "collection".to_string(),
+        orders: vec![Uint128::from(10u64)],
+        swap_params: swap_params.clone(),
+    };
+    TestExecuteMsg::SwapNftsForTokens {
+        collection: "collection".to_string(),
+        nft_orders: nft_orders.clone(),
+        swap_params: swap_params.clone(),
+    };
+
+    let test = TestExecuteMsg::Foo {};
+
+    // If this compiles we have won.
+    let result = match test {
+        TestExecuteMsg::Foo
+        | TestExecuteMsg::Bar(_)
+        | TestExecuteMsg::Baz { .. }
+        | TestExecuteMsg::SwapTokensForAnyNfts { .. }
+        | TestExecuteMsg::SwapNftsForTokens { .. } => "yay",
     };
     assert_eq!(result, "yay");
 }
