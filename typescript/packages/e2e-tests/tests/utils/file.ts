772fb6c4054115b8f5c335b37c648f2e67b16fe2
Tasio Victoria <tasiovictoria@gmail.com>
2023-09-29 13:29:03 -0400

    Fix instantiate2 builder tests

--- typescript/packages/e2e-tests/tests/utils/file.ts
+++ typescript/packages/e2e-tests/tests/utils/file.ts
@@ -3,18 +3,22 @@ import readline from 'readline'
 
 export const readChecksumFile = async (checksumFilePath: string, target: string): Promise<string> => {
   return new Promise((resolve, reject) => {
+    let retval: string
     const readInterface = readline.createInterface(fs.createReadStream(checksumFilePath))
 
     readInterface.on('line', function (line) {
       const [checksum, fileName] = line.split(/\s+/) // Split by whitespace
-      if (fileName === target) {
+      if (fileName.trim() === target) {
+        retval = checksum
+        resolve(retval)
         readInterface.close() // Close the stream if the checksum is found
-        resolve(checksum)
       }
     })
 
     readInterface.on('close', function () {
-      reject(`Checksum for ${target} not found.`)
+      if (!retval) {
+        reject(`Checksum for ${target} not found.`)
+      }
     })
   })
 }
