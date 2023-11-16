95befc2028676279cafb6e68ad713fb5d57c42dd
Tasio Victoria <tasiovictoria@gmail.com>
2023-11-16 10:50:43 -0500

    Add linting to infinity-swap pkg

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  root: true,
+  extends: ['@uju-labs/eslint-config-custom/library'],
+};
--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
-import _ from "lodash"
-import path from "path"
-import fs from "fs"
+import path from 'node:path';
+import fs from 'node:fs';
+import _ from 'lodash';
 
 export const readChecksumFile = async (): Promise<Record<string, string>> => {
-  const checksumMap: Record<string, string> = {}
-  const checksumFilePath = path.resolve(__dirname, "./artifacts/checksums.txt")
-  const checksumFile = Bun.file(checksumFilePath)
-  const fileText = await checksumFile.text()
-  const lines = fileText.split("\n")
+  const checksumMap: Record<string, string> = {};
+  const checksumFilePath = path.resolve(__dirname, './artifacts/checksums.txt');
+  const checksumFile = Bun.file(checksumFilePath);
+  const fileText = await checksumFile.text();
+  const lines = fileText.split('\n');
   for (const line of lines) {
-    const [checksum, fileName] = line.split(/\s+/)
+    const [checksum, fileName] = line.split(/\s+/);
     if (!checksum || !fileName) {
-      continue
+      continue;
     }
-    checksumMap[checksum] = fileName
+    checksumMap[checksum] = fileName;
   }
-  return checksumMap
-}
+  return checksumMap;
+};
 
-export async function readWasmFile(fileName: string) {
-  const wasmFile = path.resolve(__dirname, "./artifacts", fileName)
-  return fs.readFileSync(wasmFile, { encoding: null })
+export function readWasmFile(fileName: string): Buffer {
+  const wasmFile = path.resolve(__dirname, './artifacts', fileName);
+  return fs.readFileSync(wasmFile, { encoding: null });
 }
